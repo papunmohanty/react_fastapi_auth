08ba33b5852ae3e4773d8aedc4c502eb61dd8eae
Papun Mohanty <papun@BOOK-Q002T004VI.localdomain>
2022-10-23 15:50:14 +0530

    backend::Add leads APIs

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import fastapi as _fastapi
 import fastapi.security as _security
 import sqlalchemy.orm as _orm
@@ -19,7 +21,9 @@ async def create_user(
             status_code=_fastapi.status.HTTP_400_BAD_REQUEST,
             detail="Email already in use",
         )
-    return await _services.create_user(user, db)
+    user = await _services.create_user(user, db)
+
+    return await _services.create_token(user)
 
 
 @app.post("/api/token")
@@ -37,7 +41,7 @@ async def generate_token(
             detail="Invalid Credentials",
         )
 
-    return await _services.create_jwt(user=user)
+    return await _services.create_token(user=user)
 
 
 @app.get("/api/users/me", response_model=_schemas.User)
@@ -45,3 +49,50 @@ async def get_user(
     user: _schemas.User = _fastapi.Depends(_services.get_current_user),
 ):
     return user
+
+
+@app.post("/api/leads", response_model=_schemas.Lead)
+async def create_lead(
+    lead: _schemas.LeadCreate,
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    return await _services.create_lead(user=user, db=db, lead=lead)
+
+
+@app.get("/api/leads", response_model=List[_schemas.Lead])
+async def get_leads(
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    return await _services.get_leads(user=user, db=db)
+
+
+@app.get("/api/leads/{lead_id}", status_code=200)
+async def get_lead(
+    lead_id: int,
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    return await _services.get_lead(lead_id, user, db)
+
+
+@app.delete("/api/leads/{lead_id}", status_code=204)
+async def delete_lead(
+    lead_id: int,
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    await _services.delete_lead(lead_id, user, db)
+    return {"message", "Successfully Deleted"}
+
+
+@app.put("/api/leads/{lead_id}", status_code=200)
+async def update_lead(
+    lead_id: int,
+    lead: _schemas.LeadCreate,
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    await _services.update_lead(lead_id, lead, user, db)
+    return {"message": "Successfully Updated"}
--- backend/services.py
+++ backend/services.py
@@ -1,3 +1,5 @@
+import datetime as _dt
+
 import fastapi as _fastapi
 import fastapi.security as _security
 import jwt as _jwt
@@ -53,7 +55,7 @@ async def authenticate_user(email: str, password: str, db: _orm.Session):
     return user
 
 
-async def create_jwt(user: _models.User):
+async def create_token(user: _models.User):
     user_obj = _schemas.User.from_orm(user)
 
     token = _jwt.encode(user_obj.dict(), JWT_SECRET)
@@ -75,3 +77,73 @@ async def get_current_user(
         )
 
     return _schemas.User.from_orm(user)
+
+
+async def create_lead(
+    user: _schemas.User, db: _orm.Session, lead: _schemas.LeadCreate
+):
+    lead = _models.Lead(**lead.dict(), owner_id=user.id)
+    db.add(lead)
+    db.commit()
+    db.refresh(lead)
+    return _schemas.Lead.from_orm(lead)
+
+
+async def get_leads(user: _schemas.User, db: _orm.Session):
+    leads = db.query(_models.Lead).filter_by(owner_id=user.id)
+
+    return list(map(_schemas.Lead.from_orm, leads))
+
+
+async def _lead_selector(lead_id: int, user: _schemas.User, db: _orm.Session):
+    """
+    Reusable lead getter function to use in various parts where lead object
+    based on lead id is needed.
+    """
+    lead = (
+        db.query(_models.Lead)
+        .filter_by(owner_id=user.id)
+        .filter(_models.Lead.id == lead_id)
+        .first()
+    )
+
+    if lead is None:
+        raise _fastapi.HTTPException(
+            status_code=404, detail="Lead does not exist"
+        )
+
+    return lead
+
+
+async def get_lead(lead_id: int, user: _schemas.User, db: _orm.Session):
+    lead = await _lead_selector(lead_id=lead_id, user=user, db=db)
+
+    return _schemas.Lead.from_orm(lead)
+
+
+async def delete_lead(lead_id: int, user: _schemas.User, db: _orm.Session):
+    lead = await _lead_selector(lead_id, user, db)
+
+    db.delete(lead)
+    db.commit()
+
+
+async def update_lead(
+    lead_id: int,
+    lead: _schemas.LeadCreate,
+    user: _schemas.User,
+    db: _orm.Session,
+):
+    lead_db = await _lead_selector(lead_id, user, db)
+
+    lead_db.first_name = lead.first_name
+    lead_db.last_name = lead.last_name
+    lead_db.email = lead.email
+    lead_db.company = lead.company
+    lead_db.note = lead.note
+    lead_db.date_last_updated = _dt.datetime.utcnow()
+
+    db.commit()
+    db.refresh(lead_db)
+
+    return _schemas.Lead.from_orm(lead_db)
