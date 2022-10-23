1b5098913eeaed9fa2cca1bcb549ddd61f3b7ee8
Papun Mohanty <papun@BOOK-Q002T004VI.localdomain>
2022-10-23 11:04:05 +0530

    Add: backend::JWT authentication

--- backend/models.py
+++ backend/models.py
@@ -22,13 +22,13 @@ class User(_database.Base):
 class Lead(_database.Base):
     __tablename__ = "leads"
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
-    owner_id = _sql.Column(_sql.Index, _sql.ForeignKey("users.id"))
+    owner_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id"))
     first_name = _sql.Column(_sql.String, index=True)
     last_name = _sql.Column(_sql.String, index=True)
     email = _sql.Column(_sql.String, index=True)
     company = _sql.Column(_sql.String, index=True, default="")
     note = _sql.Column(_sql.String, default="")
     date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
-    date_last_updated = _sql.Column(_sql.DateTime, dafault=_dt.datetime.utcnow)
+    date_last_updated = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
 
     owner = _orm.relationship("User", back_populates="leads")
